e8d3ada603efcffe311123c99a5e3c2cca6bb3ff
Rick Mercer <rhm12399@users.noreply.github.com>
2017-09-05 14:18:58 -0700

    Added @Tests to check Intermediate Strategy

--- tests/TestStrategies.java
+++ tests/TestStrategies.java
@@ -1,6 +1,6 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.awt.Point;
 
@@ -13,51 +13,80 @@ import model.IntermediateAI;
 import model.TicTacToeGame;
 
 public class TestStrategies {
-   
+
+ 
+  @Test
+  public void testIntermediateToWin() {
+    TicTacToeGame theGame = new TicTacToeGame();
+    // When testing is true, humanMove does not set the computer player move
+    theGame.humanMove(0, 0, true); // X
+    theGame.computerMove(2, 0); // O
+    theGame.humanMove(1, 0, true); // X
+    theGame.computerMove(2, 1); // O
+    theGame.humanMove(1, 0, true); // X
+
+    //  System.out.println(theGame + "\n" + computerMove);
+    //  X  _  _ 
+    //  X  _  _ 
+    //  O  O   
+    // Should get Point(2,2)
+    ComputerPlayer playerWithIntermediateStrategy = new ComputerPlayer();
+    playerWithIntermediateStrategy.setStrategy(new IntermediateAI());
+    Point computerMove = playerWithIntermediateStrategy.desiredMove(theGame);
+    theGame.computerMove(computerMove.x, computerMove.y);
+    assertEquals(2, computerMove.x);
+    assertEquals(2, computerMove.y);
+  }
+
+  @Test
+  public void testIntermediateToBlock() {
+    TicTacToeGame theGame = new TicTacToeGame();
+    // When testing is true, humanMove does not set the computer player move
+    theGame.humanMove(0, 0, true); // X
+    theGame.computerMove(2, 0); // O
+    theGame.humanMove(0, 1, true); // X
+
+    // System.out.println(theGame + "\n" +computerMove);
+    // X  X  O 
+    //  _  _  _ 
+    //  O  _  _ 
+    // Should get Point(0,2) to block
+    ComputerPlayer playerWithIntermediateStrategy = new ComputerPlayer();
+    playerWithIntermediateStrategy.setStrategy(new IntermediateAI());
+    Point computerMove = playerWithIntermediateStrategy.desiredMove(theGame);
+    theGame.computerMove(computerMove.x, computerMove.y);
+    assertEquals(0, computerMove.x);
+    assertEquals(2, computerMove.y);
+  }
+
   @Test(expected = IGotNowhereToGoException.class)
   public void showWhatHappensWhenAnExceptionMustBeThrown() {
     TicTacToeGame theGame = new TicTacToeGame();
-
-    ComputerPlayer playerWithRandomStrategy = new ComputerPlayer();
-    playerWithRandomStrategy.setStrategy(new RandomAI());
-
-    // Make one move
-    Point aRandomSquare = playerWithRandomStrategy.desiredMove(theGame);
-    theGame.humanMove(aRandomSquare.x, aRandomSquare.y, true);
-
-    // and another
-    aRandomSquare = playerWithRandomStrategy.desiredMove(theGame);
-    theGame.humanMove(aRandomSquare.x, aRandomSquare.y, true);
-    // and another
-    aRandomSquare = playerWithRandomStrategy.desiredMove(theGame);
-    theGame.humanMove(aRandomSquare.x, aRandomSquare.y, true);
-    // and another
-    aRandomSquare = playerWithRandomStrategy.desiredMove(theGame);
-    theGame.humanMove(aRandomSquare.x, aRandomSquare.y, true);
-    // and another
-    aRandomSquare = playerWithRandomStrategy.desiredMove(theGame);
-    theGame.humanMove(aRandomSquare.x, aRandomSquare.y, true);
-    // and another 
-    aRandomSquare = playerWithRandomStrategy.desiredMove(theGame);
-    theGame.humanMove(aRandomSquare.x, aRandomSquare.y, true);
-    // and another
-    aRandomSquare = playerWithRandomStrategy.desiredMove(theGame);
-    theGame.humanMove(aRandomSquare.x, aRandomSquare.y, true);
-    // and an eight 
-    aRandomSquare = playerWithRandomStrategy.desiredMove(theGame);
-    theGame.humanMove(aRandomSquare.x, aRandomSquare.y, true);
-    // and the ninth
-    aRandomSquare = playerWithRandomStrategy.desiredMove(theGame);
-    theGame.humanMove(aRandomSquare.x, aRandomSquare.y, true);
-
-    // This should throw an exception since the board is filled 
-    aRandomSquare = playerWithRandomStrategy.desiredMove(theGame);
-  }
+    theGame.humanMove(0, 0, true); // X
+    theGame.computerMove(0, 1); // O
+    theGame.humanMove(1, 0, true); // X
+    theGame.computerMove(1, 1); // O
+    theGame.humanMove(0, 2, true); // X
+    theGame.computerMove(1, 2); // O
+    theGame.humanMove(2, 1, true); // X
+    theGame.computerMove(2, 0); // O
+    theGame.humanMove(2, 2, true); // X
+    
+    assertTrue(theGame.tied());
+//    System.out.println(theGame + "\n" );
+//    X  O  X 
+//    X  O  O 
+//    O  X  X 
  
+    // This should throw an exception since the board is filled 
+    ComputerPlayer player = new ComputerPlayer();
+    player.setStrategy(new RandomAI());
+    // This should throw an exception
+    Point tryToMakeMoveWhenFilled = player.desiredMove(theGame);
+  }
+
   @Test
   public void run1000TicTacToeGames() {
-    // This is currently an infinite loop since the two strategies
-    // always return the same Point with .x and .y
     ComputerPlayer randomBot = new ComputerPlayer();
     randomBot.setStrategy(new RandomAI());
     ComputerPlayer IntermediateBot = new ComputerPlayer();
@@ -82,8 +111,8 @@ public class TestStrategies {
     System.out.println("strategy choose first.  Ties can certainly happen.");
     System.out.println("===========================================");
     System.out.println("Intermediate win percentage: " + IntermediatePlayerWins / 10. + "%");
-    System.out.println("Random win percentage: " + randomPlayerWins/ 10. + "%");
-    System.out.println("Ties happened " + ties/ 10. + "% of the games");
+    System.out.println("Random win percentage: " + randomPlayerWins / 10. + "%");
+    System.out.println("Ties happened " + ties / 10. + "% of the games");
   }
 
   private char playOneGame(ComputerPlayer first, ComputerPlayer second) {
@@ -112,22 +141,6 @@ public class TestStrategies {
       if (theGame.didWin('O'))
         return 'O';
     }
-  }
-
-  @Test
-  public void testIntermediate() {
-    TicTacToeGame theGame = new TicTacToeGame();
-
-    theGame.humanMove(0, 0, true); // X
-    theGame.computerMove(2, 0); // O
-    theGame.humanMove(0, 1, true); // X
-
-    ComputerPlayer playerWithIntermediateStrategy = new ComputerPlayer();
-    playerWithIntermediateStrategy.setStrategy(new IntermediateAI());
-    Point computerMove = playerWithIntermediateStrategy.desiredMove(theGame);
-    theGame.computerMove(computerMove.x, computerMove.y);
-    assertEquals(0, computerMove.x);
-    assertEquals(2, computerMove.y);
   }
 
   @Test
